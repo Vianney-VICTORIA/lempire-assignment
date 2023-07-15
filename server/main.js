c9a94fc1eb208611683cd28527f56ac59246a6f0
viannneyvictoria <vianneyvictoria@gmail.com>
2023-07-15 18:10:52 +1200

    💽 CRUD mongoDB

--- server/main.js
+++ server/main.js
@@ -1,6 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import { TasksCollection } from '../imports/api/TasksCollection';
 
+Meteor.publish('ServerPublication', function publishFunction() {
+    return TasksCollection.find();
+});
 const insertTask = taskText => TasksCollection.insert({ text: taskText });
 
 Meteor.startup(() => {
